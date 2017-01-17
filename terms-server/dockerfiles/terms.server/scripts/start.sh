7af475501b0ee29899e52a1f52a062fee432046d
Enrique Pérez Arnaud <enrique@cazalla.net>
2017-01-17 18:43:37 +0100

    working?

---
+++ terms-server/dockerfiles/terms.server/scripts/start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Stop on error
+set -e
+
+if [[ -e /firstrun ]]; then
+
+  echo "Running zc.buildout..."
+  echo $( /opt/tserve-buildout/bin/buildout 2>&1 )
+
+  echo "Initializing knowledge base..."
+  echo $( /opt/tserve-buildout/bin/initterms -c /opt/tserve-buildout/etc/terms.cfg 2>&1 )
+
+  echo "Finishing first run..."
+  rm /firstrun
+fi
+
+# Start Terms
+echo "Starting Terms Server..."
+cd /opt/tserve-buildout
+
+exec start-stop-daemon --start --exec \
+      /opt/tserve-buildout/bin/webserver -- -c /opt/tserve-buildout/etc/terms.cfg
